a3d6e4973cce428a7ef4970ecf1d928a9c0fdd75
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-02-02 20:58:09 -0500

    Added yaml file for file directories

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,19 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-
   require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
+  let config = grunt.file.readYAML('Gruntconfig.yml');
+
+    grunt.initConfig({
     sass: {
       dist: {
-        src: 'src/sass/style.scss',
-        dest: 'dist/css/style.css'
+        src: config.scssSrcDir + 'style.scss',
+        dest: config.cssDir + 'style.css'
       }
     },
     concat: {
       dist: {
-        src: 'src/js/*.js',
-        dest: 'dist/js/app.js'
+        src: config.jsSrcDir + '*.js',
+        dest: config.jsConcatDir + 'app.js'
       }
 
     }
